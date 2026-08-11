eb13241592a0e451415f5772154b12aa8de7c34c
temoor <temoor_abas@yahoo.com>
2026-08-12 00:27:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon="🏠",
     layout="wide"
 )
-st.sidebar.title("🏠 Home")
+
 # ==========================================
 # TITLE
 # ==========================================
