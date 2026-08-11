d265cc2a04286f11d6c5cd8fca29ff5a99072630
temoor <temoor_abas@yahoo.com>
2026-08-12 00:20:00 +0500

    Update app.py
    Change app sidebar name to Home

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import plotly.express as px
 # ==========================================
 st.set_page_config(
     page_title="AI Data Analysis Agent",
-    page_icon="🤖",
+    page_icon="🏠",
     layout="wide"
 )
-
+st.sidebar.title("🏠 Home")
 # ==========================================
 # TITLE
 # ==========================================
